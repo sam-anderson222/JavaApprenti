55b00a53aa627d5a079868e1d2865b2c7ca24d34
sam-anderson222 <samlanderson222@gmail.com>
2025-06-06 12:22:51 -0500

    start of battleship game

---
+++ Battleships/src/MainGameLogic.java
@@ -0,0 +1,15 @@
+public class MainGameLogic {
+
+    /*
+    Battleship
+    Board (Made up of 5 ships of differing sizes on 10x10 board)
+    Tiles (Makes up the board, can have a ship or be hit.)
+    Ships (A ship with a length, position, and rotation on the board. One of 5 types)
+    Players (Each player has their own board with their own orientation of their ships)
+    Game logic (Each player takes turns guessing where the other player ships are)
+
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Battleships/src/Tile.java
@@ -0,0 +1,25 @@
+public class Tile {
+    private final boolean shipTile; // Is there a part of a ship on this tile?
+    private boolean hitTile; // Has this tile been guessed by the opposing player?
+
+    public Tile(boolean shipTile, boolean hitTile) {
+        this.shipTile = shipTile;
+        this.hitTile = hitTile;
+    }
+
+    // Ships can't move after being placed, so there is no setter function for ships.
+    public boolean getIsShipTile() {
+        return shipTile;
+    }
+
+    // Getter / setter for if the hit has been guessed / hit by the opposing player.
+    public void setHitTile(boolean hitTile) {
+        this.hitTile = hitTile;
+    }
+
+    public boolean getHitTile() {
+        return hitTile;
+    }
+
+
+}
