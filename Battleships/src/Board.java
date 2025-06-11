133f045f2d1e622f581604d8aa23247a08f36085
sam-anderson222 <samlanderson222@gmail.com>
2025-06-11 12:18:03 -0500

    Progress battleships

--- Battleships/src/Board.java
+++ Battleships/src/Board.java
@@ -1,16 +1,23 @@
+import java.util.Random;
+
 public class Board {
     private Tile[][] gameBoard;
 
     // default board size is 10x10
     public Board() {
+        Random rng = new Random();
         gameBoard = new Tile[10][10];
 
         // Give a Tile object to each place on the board.
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                gameBoard[i][j] = new Tile(false,false);
+                gameBoard[i][j] = new Tile(rng.nextBoolean(),rng.nextBoolean());
             }
         }
+    }
+
+    public Tile getGameTile(char column, int row) {
+        return null;
     }
 
     public Tile[][] getGameBoard() {
--- Battleships/src/BoardUtils.java
+++ Battleships/src/BoardUtils.java
@@ -3,16 +3,40 @@ public class BoardUtils {
 
     // Takes a 2D array of tiles and prints the game board state.
     public static void printBoard(Tile[][] bd) {
-        for (int i = 0; i < bd.length; i++) {
-            for (int j = 0; j < bd.length; j++) {
-                Tile currentTile = bd[i][j];
-                if (currentTile.getIsShipTile() && currentTile.getHitTile()) { // If a tile with a ship on has been guessed
-                    System.out.print("H");
-                } else if (currentTile.getHitTile()) { // If a tile without a shipped has been guessed.
+        // Prints the header (A, B, C, D, ...)
+        printHeader(bd.length);
 
-                }
+        // Print blips for each tile on the game board.
+        for (int i = 0; i < bd.length; i++) {
+            System.out.printf("%-2d | ", i+1); // Prints numbers for each row
+            for (int j = 0; j < bd.length; j++) {
+                printTile(bd[i][j]);
             }
-            System.out.println();
+            System.out.println(); // Print newline character
+        }
+    }
+
+    // Prints the (A, B, C, D, E, ...) header
+    private static void printHeader(int numColumns) {
+        System.out.print("    ");
+        char header = 'A';
+        for (int k = 0; k < numColumns; k++) {
+            System.out.printf(" %c  ", header);
+            header++;
+        }
+        System.out.println();
+    }
+
+    // Prints a different symbol depending on the tile state. Is there a ship? has the tile been guessed? was the guess a hit or miss?
+    private static void printTile(Tile currentTile) {
+        if (currentTile.getIsShipTile() && currentTile.getHitTile()) { // If a tile with a ship on has been guessed
+            System.out.print("H | "); // H for Hit
+        } else if (currentTile.getHitTile()) { // If a tile without a shipped has been guessed.
+            System.out.print("M | "); // M for Miss
+        } else if (currentTile.getIsShipTile()) {
+            System.out.print("S | "); // S for Ship (For looking at one's own board)
+        } else {
+            System.out.print("* | "); // * for an unguessed tile.
         }
     }
 }
