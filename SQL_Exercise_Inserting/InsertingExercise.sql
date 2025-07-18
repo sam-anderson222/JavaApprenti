d5d6efe34d9f982d983a917359cc541f574f7619
sam-anderson222 <samlanderson222@gmail.com>
2025-07-18 16:25:27 -0500

    completed inserting exercise

---
+++ SQL_Exercise_Inserting/InsertingExercise.sql
@@ -0,0 +1,33 @@
+USE sql_exercises;
+
+-- Part 1
+-- INSERT INTO customer(CustomerName) VALUES
+-- ('Emma Rivera'),
+-- ('Noah Gray');
+
+-- INSERT INTO customerorder(CustomerId, OrderDate) VALUES
+-- (1, '2025-07-18'),
+-- (2, '2025-07-18');
+
+
+-- Part 2
+-- INSERT INTO payment(OrderId, PaymentMethodId, Amount) VALUES
+-- (1, 1, 49.99);
+
+-- Part 3
+-- I got an Error Code: 1452, foreign key 
+-- INSERT INTO payment(OrderId, PaymentMethodId, Amount) VALUES
+-- (1, 999, 49.99);
+
+-- Part 4
+-- INSERT INTO customer(CustomerName) VALUES
+-- ('Liam Davis'),
+-- ('Olivia Brooks'),
+-- ('Sophia Martinez');
+
+-- INSERT INTO customerorder(CustomerId, OrderDate) VALUES
+-- (3, '2025-07-18'),
+-- (4, '2025-07-18'),
+-- (5, '2025-07-18');
+
+
