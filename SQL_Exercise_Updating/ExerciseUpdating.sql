b76db6f290cf46c5e906d5f299682fb8dac160dd
sam-anderson222 <samlanderson222@gmail.com>
2025-07-18 16:49:16 -0500

    Completed updating exercise

---
+++ SQL_Exercise_Updating/ExerciseUpdating.sql
@@ -0,0 +1,14 @@
+USE sql_exercises;
+
+-- (1)
+-- UPDATE product SET Price = 6.25 WHERE ProductId = 101;
+
+-- (2)
+-- UPDATE product SET ProductName = 'Executive Pen Set', Price = 8.25 WHERE ProductId = 102;
+
+-- (3)
+-- UPDATE product SET EndDate = '2024-12-31' WHERE CategoryId = 2;
+
+-- (4)
+-- Got an Error Code:1452
+-- UPDATE product SET CategoryId = 3 WHERE ProductId = 103;
